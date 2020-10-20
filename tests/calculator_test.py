717c884fd2793ec7c335a5fff495015984ecd7c9
Chris Kite <chris.kat82@googlemail.com>
2020-10-20 22:21:07 +0100

    initial homework commit

---
+++ tests/calculator_test.py
@@ -0,0 +1,15 @@
+import unittest
+from src.calculator import add, divide, multiply, subtract
+
+class TestCalculator(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(5, add(2, 3))
+
+    def test_subtract(self):
+        self.assertEqual(3, subtract(10, 7))
+
+    def test_divide(self):
+        self.assertEqual(10, divide(100, 10))
+
+    def test_multiply(self):
+        self.assertEqual(55, multiply(11, 5))
